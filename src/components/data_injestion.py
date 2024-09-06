7748ef60910e9d1cfb625e52c8fe4d9b7d9440d8
Debopam Param <debopamwork@gmail.com>
2024-09-06 19:49:22 +0530

    Model Test and Validations

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -7,25 +7,30 @@ from src.logger import logging
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
+
 @dataclass
 class DataIngestionConfig:
-    ''' Data Ingestion Configuration 
-        We need to create all artifacts because, we may be reading them from an external data source
-    '''
+    """Data Ingestion Configuration
+    We need to create all artifacts because, we may be reading them from an external data source
+    """
+
     train_data_path: str = os.path.join("artifacts", "train.csv")
     test_data_path: str = os.path.join("artifacts", "test.csv")
     raw_data_path: str = os.path.join("artifacts", "raw_data.csv")
 
+
 class DataIngestion:
     def __init__(self):
         self.config = DataIngestionConfig()
-    
+
     def initiate_data_ingestion(self):
         logging.info("Initiating Data Ingestion")
         try:
-            df = pd.read_csv('src/data/HREmployee_data.csv')
+            df = pd.read_csv("src/data/IBMEmployee_data.csv")
             logging.info("Data Ingestion Successful")
-            os.makedirs(os.path.join(os.path.dirname(self.config.raw_data_path)), exist_ok=True)
+            os.makedirs(
+                os.path.join(os.path.dirname(self.config.raw_data_path)), exist_ok=True
+            )
             df.to_csv(self.config.raw_data_path, index=False, header=True)
 
             logging.info("Train Test Split Initiated")
@@ -34,7 +39,8 @@ class DataIngestion:
             test.to_csv(self.config.test_data_path, index=False, header=True)
             logging.info("Train Test Split Successful")
         except Exception as e:
-            raise CustomException(e,sys) from e
+            raise CustomException(e, sys) from e
+
 
 if __name__ == "__main__":
     obj = DataIngestion()
---
+++ src/components/data_transformation.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
